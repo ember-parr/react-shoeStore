599da2a32ce650bec1d963f9cb48f255b215f06b
Ember P <ember@emberparr.com>
2021-06-02 15:57:12 -0500

    added cart btn on detail page with navigate to cart

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Spinner from "./Spinner";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./services/useFetch";
 import PageNotFound from "./PageNotFound"
+
 export default function Detail() {
     const { id } = useParams()
     const { data: product, loading, error } = useFetch(`products/${id}`)
-
+    const navigate = useNavigate()
     // start of guard clauses (checking for correct information)
-    if (error) throw error;
     if (loading) return <Spinner />
-    if (!id) return <PageNotFound />
+    if (!product) return <PageNotFound />
+    if (error) throw error;
     // end of guard clauses 
     //   return <h1>Detail</h1>;
     console.log("productId: ", id)
@@ -21,6 +22,7 @@ export default function Detail() {
         <h1>{product.name}</h1>
         <p>{product.description}</p>
         <p id="price">${product.price}</p>
+        <p><button class="btn btn-primary" onClick={() => navigate('/cart') }>Add To Cart</button></p>
         <img src={`/images/${product.image}`} alt={product.category} />
         </div>
     );
